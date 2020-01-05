c1196235c5d5c9c0e430db68481fd14a3491a4db
Kutay <skutaykilic@gmail.com>
2020-01-05 20:22:39 +0300

    Refactored the code and added comments

--- decision_tree.py
+++ decision_tree.py
@@ -21,25 +21,28 @@ def main():
 		training_data, validation_data, test_data = shuffleAndSplitToTrainingValidationTest(data)
 		decision_tree = DecisionTree(training_data, validation_data, test_data, EntropyCalculator.calculateEntropyWithInformationGainFormula)
 		decision_trees_with_information_gain.append(decision_tree)
-	# 10 times with information gini impurity formula
+	# 10 times with gini impurity formula
 	decision_trees_with_gini_impurity = []
 	for i in range(ITERATION_COUNT):
 		training_data, validation_data, test_data = shuffleAndSplitToTrainingValidationTest(data)
 		decision_tree = DecisionTree(training_data, validation_data, test_data, EntropyCalculator.calculateEntropyWithGiniImpurityFormula)
 		decision_trees_with_gini_impurity.append(decision_tree)
+	# report the results
 	OutputUtility.reportLossInTestSet(decision_trees_with_information_gain, decision_trees_with_gini_impurity)
 	OutputUtility.reportMeanAndVariancesOfLosses(decision_trees_with_information_gain, decision_trees_with_gini_impurity)
 	OutputUtility.plotLossRates(decision_trees_with_information_gain, decision_trees_with_gini_impurity)
 
+# reads the data and convert it to Datum objects
 def readData():
 	data = []
 	with open(DATA_FILE, "r") as f:
 		for line in f.read().splitlines():
-			row = line.split(',')
-			if len(row) > 1: # if not an empty line
+			if len(line) > 1: # if not an empty line
+				row = line.split(',')
 				data.append(Datum([float(num) for num in row[:-1]], row[-1]))
 	return data
 
+# Split the data into training(20%), validation(40%), and test (40%)
 def shuffleAndSplitToTrainingValidationTest(data):
 	random.shuffle(data)
 	tenpercent_n = len(data) // 10
@@ -62,6 +65,8 @@ class DecisionTree:
 		self.root = DecisionTreeNode(training_data[:])
 		self.buildTree()
 
+	# Build the tree in bfs order
+	# If the splitting is not rational with respect to validation data then it starts to stop the splittings
 	def buildTree(self):
 		current_error_rate = self.computeValidationError()
 		q = collections.deque()
@@ -69,13 +74,14 @@ class DecisionTree:
 		while q:
 			self.training_loss_of_depths.append(self.computeTrainingError())
 			self.validation_loss_of_depths.append(self.computeValidationError())
+			# do layer by layer traversel since I am also interested in the loss rates of layers
 			level_size = len(q)
 			for i in range(level_size):
 				node = q.popleft()
 				if node.isSuitableToSplit():
 					param_index, param_value = self.findBestSplit(node.data)
 					node.split_param_index, node.split_param_value = param_index, param_value
-					left_data, right_data = self.computeSplitWithAttributeAndValue(node.data, param_index, param_value)
+					left_data, right_data = self.splitWithAttributeAndValue(node.data, param_index, param_value)
 					node.left, node.right = DecisionTreeNode(left_data), DecisionTreeNode(right_data)
 					new_error_rate = self.computeValidationError()
 					if new_error_rate < current_error_rate: # We can hold the split
@@ -85,7 +91,7 @@ class DecisionTree:
 					else: # The split is not rational because error rate has increased
 						node.left, node.right = None, None # Recover children
 
-
+	# Finds the best split paramater and it's value which gives min entropy
 	def findBestSplit(self, data):
 		#print([vars(datum) for datum in data])
 		best_entropy = 999
@@ -93,7 +99,7 @@ class DecisionTree:
 		best_param_value = None
 		for param_index in range(len(data[0].params)):
 			for datum in data:
-				left_data, right_data = self.computeSplitWithAttributeAndValue(data, param_index, datum.params[param_index])
+				left_data, right_data = self.splitWithAttributeAndValue(data, param_index, datum.params[param_index])
 				entropy = self.computeEntropyAfterSplitting(left_data, right_data)
 				if entropy < best_entropy:
 					best_entropy = entropy
@@ -101,7 +107,8 @@ class DecisionTree:
 					best_param_value = datum.params[param_index]
 		return (best_param_index, best_param_value)
 
-	def computeSplitWithAttributeAndValue(self, data, param_index, param_value):
+	# Split the data into two parts with respect to the given splitting point
+	def splitWithAttributeAndValue(self, data, param_index, param_value):
 		left_data = []
 		right_data = []
 		for datum in data:
@@ -111,6 +118,7 @@ class DecisionTree:
 				right_data.append(datum)
 		return (left_data, right_data)
 
+	# Compute the average entropy of left and right data
 	def computeEntropyAfterSplitting(self, left_data, right_data):
 		left_entropy = self.entropy_calculator_function(left_data)
 		right_entropy = self.entropy_calculator_function(right_data)
@@ -123,7 +131,7 @@ class DecisionTree:
 	def computeTrainingError(self):
 		return DecisionTreeEvaluator.computeErrorPercentage(self.root, self.training_data)
 
-
+# An extended Binary Tree Node to be used in the decision tree
 class DecisionTreeNode:
 	def __init__(self, data):
 		self.data = data
@@ -132,20 +140,23 @@ class DecisionTreeNode:
 		self.right = None
 		self.split_param_index = None
 		self.split_param_value = None 
-		
+	
+	# Returns the class that is most frequent in the data of node	
 	def findPluratiyClass(self):
 		classes = [datum.classs for datum in self.data]
 		return max(set(classes), key = classes.count)
 
+	# The node is suitable to split if it has at least two nodes and has different class datums
 	def isSuitableToSplit(self):
-		return len(self.data) > 2 and len(set([datum.classs for datum in self.data])) > 1
+		return len(self.data) >= 2 and len(set([datum.classs for datum in self.data])) > 1
 
-
+# Object representation of datums
 class Datum:
 	def __init__(self, params, classs):
 		self.params = params
 		self.classs = classs
 
+# Utility function to compute the errors, predict the result, etc by using a decision tree
 class DecisionTreeEvaluator:
 	@staticmethod
 	def computeErrorPercentage(decision_tree_root, data):
@@ -168,6 +179,8 @@ class DecisionTreeEvaluator:
 				current_node = current_node.right
 		return current_node.pluratiy_class
 
+# This module contains two different entropy calculation formulas, which are:
+# Information Gain and Gini Impurity
 class EntropyCalculator:
 	@staticmethod
 	def calculateEntropyWithInformationGainFormula(data):
@@ -193,6 +206,8 @@ class EntropyCalculator:
 			entropy -= proportion * proportion
 		return entropy
 
+# This module contains utility functions to print and plot the necessary outputs, such as:
+# Loss rates of iteration, mean and variance of test data losses, and plots of loss rates with respect to decision tree depth
 class OutputUtility:
 	@staticmethod
 	def reportLossInTestSet(decision_trees_with_information_gain, decision_trees_with_gini_impurity):
